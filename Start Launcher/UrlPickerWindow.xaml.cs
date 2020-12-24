2337a18cdfc03698ceb88d6608d694ee470b4580
Piotr Kowalski <Piotr.Kowalski.1998@protonmail.com>
2020-12-24 13:21:31 +0100

    SL-13 Add adding urls

---
+++ Start Launcher/UrlPickerWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StartLauncher
+{
+    /// <summary>
+    /// Interaction logic for UrlPicker.xaml
+    /// </summary>
+    public partial class UrlPickerWindow : Window
+    {
+        public string Url { get; set; }
+        public bool Confirmed { get; set; }
+        public UrlPickerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmUrl_Click(object sender, RoutedEventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(UrlText.Text, UriKind.Absolute))
+            {
+                Confirmed = true;
+                Url = UrlText.Text;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid url");
+            }
+        }
+    }
+}
