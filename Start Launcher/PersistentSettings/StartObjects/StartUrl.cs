68787995ff81a56c3e712a45be434ee78c037243
Piotr Kowalski <Piotr.Kowalski.1998@protonmail.com>
2020-12-24 13:01:26 +0100

    SL-11 Add saving launch urls

---
+++ Start Launcher/PersistentSettings/StartObjects/StartUrl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StartLauncher.PersistentSettings.StartObjects
+{
+    public class StartUrl : StartObject
+    {
+        public StartUrl()
+        {
+
+        }
+        public StartUrl(string location, int launchOrder) : base(location, location, launchOrder)
+        {
+            Validate();
+        }
+        public StartUrl(string location, string userGivenName, int launchOrder) : base(location, userGivenName, launchOrder)
+        {
+            Validate();
+        }
+
+        public void Validate()
+        {
+            if (!Uri.IsWellFormedUriString(Location, UriKind.Absolute))
+            {
+                throw new ArgumentException("InvalidUrl");
+            }
+            if (!Location.StartsWith("http", StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new ArgumentException("Only HTTP protocol is allowed");
+            }
+        }
+
+        public override Task<bool> Run() => Task.Run(() =>
+        {
+            try
+            {
+                _ = System.Diagnostics.Process.Start(Location);
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        });
+
+        internal override void AddListToSettings(Settings settings)
+        {
+            settings.startUrls.Add(this);
+        }
+    }
+}
